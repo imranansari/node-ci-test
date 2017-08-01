341ae427ec010d93a35baa0d14a373ce1ed7a5e3
Imran Ansari <iansari@apple.com>
2017-08-01 15:02:02 -0700

    cleanup

--- ci.sh
+++ ci.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
+#GIT_URL=$(git config --get remote.origin.url)
+GIT_URL="https://github.com/imranansari/node-ci-test.git"
 
 function build(){
   echo "npm install ...."
-  cd workspace/test-node-module/ \
+  cd workspace/node-ci-test/ \
   && npm install
 }
 
 function checkout(){
     echo "checkout ...."
+    #mkdir ~/.ssh/ && ssh-keyscan -H github.com >> ~/.ssh/known_hosts
     mkdir workspace
     echo ${BASEDIR}/workspace
-    (cd workspace &&  git clone git://github.com/imranansari/test-node-module.git)
+    (cd workspace &&  git clone ${GIT_URL})
 }
 
 function pipeline(){
@@ -24,7 +27,7 @@ function dockerBuildPipeline(){
     if [ -e Dockerfile ] 
     then
         echo "Dockerfile present, doing docker build"
-        //docker build -t myimage .
+        docker build -t myimage .
     fi
         echo "Dockerfile not present, building using node:onbuild"
         docker run -ti --rm --name build-app -v $PWD:/usr/src/app/ -w \
